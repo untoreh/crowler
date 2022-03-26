2bdcc1ee7e38729ee9d6a8ba22119cd34b66adf4
untoreh <francesco.giannelli@yahoo.it>
2022-03-26 19:31:18 +0100

    dir local var

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,4 +5,5 @@
          (eval . (setq pyvenv-activate
                        (my/concat-path
                         (python-repl-current-project-dir)
-                        ".venv"))))))
+                        ".venv")))
+         (nim-compile-default-command . ('("r" "-r" "--gc:arc" "--threads:on" "--deepcopy:on" "--verbosity:0" "--hint[Processing]:off" "--excessiveStackTrace:on"))))))

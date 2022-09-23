6ac9b05c549f48733555337c8f9fb19544fcf7fb
untoreh <francesco.giannelli@yahoo.it>
2022-09-23 15:21:48 +0200

    fix: minor

--- .dir-locals.el
+++ .dir-locals.el
@@ -17,7 +17,9 @@
                            ;; (my/concat-path (getenv "HOME") ".pyenv/versions/3.8.7/lib/libpython3.8d.so")
                            )))))
  (nim-mode . ((nim-compile-default-command . ("r" "-r" "--mm:orc"))
-              (lsp-nim-nimsuggest-mapping . [(:projectFile "tests/all.nim" :fileRegex "tests/.*\\.nim")
-                                             (:projectFile "src/nim/server.nim" :fileRegex "src/nim/.*|.*.nim$")])
+              (lsp-nim-project-mapping . [(:projectFile "tests/all.nim" :fileRegex "tests/.*\\.nim"
+                                          :projectFile "src/nim/server.nim" :fileRegex "src/nim/.*|.*\\.nim"
+                                          :projectFile "src/nim/test.nim" :fileRegex "src/nim/test.*\\.nim")
+                                             ])
               (projectile-project-compilation-cmd . "nim r src/nim/server.nim")))
  )
--- Dockerfile
+++ Dockerfile
@@ -55,7 +55,7 @@ VOLUME ["/site/data"]
 FROM siteenv AS sitedeps1
 # install nimterop separately
 ARG CLEARCACHE=1
-RUN cd /; nimble install -y nimterop -d
+RUN cd /; nimble install -y nimterop
 RUN cd /
 RUN cd /site; \
     tries=3; \

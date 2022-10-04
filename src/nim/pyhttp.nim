f63b5ec3b5d246e806cd3fe972dd6d9487ad71e4
untoreh <francesco.giannelli@yahoo.it>
2022-10-04 10:13:31 +0200

    fix: uniqueness for pyhttp

--- src/nim/pyhttp.nim
+++ src/nim/pyhttp.nim
@@ -1,4 +1,5 @@
 import nimpy
+import std/monotimes
 import chronos
 import chronos/timer
 
@@ -36,43 +37,46 @@ proc pyReqGet(url: string): Future[string] {.async.} =
   withPyLock:
     result = resp.to(string)
 
-var queue: LockDeque[string]
-var httpResults: LockTable[string, string]
+var queue: LockDeque[(MonoTime, string)]
+var httpResults: LockTable[(MonoTime, string), string]
 var handler: ptr Future[void]
 
-proc requestTask(url: string) {.async.} =
+proc requestTask(t: MonoTime, url: string) {.async.} =
   var v: string
   try:
     v = await pyReqGet(url)
   except CatchableError:
     discard
-  httpResults[url] = v
+  httpResults[(t, url)] = v
 
 
 import locktplutils
 proc requestHandler() {.async.} =
-  var url: string
+  var
+    t: MonoTime
+    url: string
   while true:
     try:
       while true:
-        url = await queue.popFirstWait()
-        asyncSpawn requestTask(url)
+        (t, url) = await queue.popFirstWait()
+        asyncSpawn requestTask(t, url)
     except Exception as e:
       warn "PyRequests handler crashed, restarting. {e[]}"
       await sleepAsync(1.seconds)
 
 proc initPyHttp*() {.gcsafe.} =
   setNil(queue):
-    initLockDeque[string]()
+    initLockDeque[(MonoTime, string)]()
   setNil(httpResults):
-    initLockTable[string, string]()
+    initLockTable[(MonoTime, string), string]()
   setNil(handler):
     create(Future[void])
   handler[] = requestHandler()
 
 proc httpGet*(url: string): Future[string] {.async, raises: [Defect].} =
-  queue.addLast(url)
-  return await httpResults.popWait(url)
+  let k = (getMonoTime(), url)
+  queue.addLast(k)
+  return await httpResults.popWait(k)
 
 when isMainModule:
   initPyHttp()

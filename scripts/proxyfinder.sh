8b5c647e8d41b5fd30ffeb586ccfc588d74feb37
untoreh <francesco.giannelli@yahoo.it>
2022-10-26 17:32:38 +0200

    prx: more endpoints
    use different endpoints for different proxy types

---
+++ scripts/proxyfinder.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+proxies_dir="data/proxies"
+
+. .venv/bin/activate || { echo "venv not found"; exit 1; }
+[ -e $proxies_dir ] ||  { echo "proxies dir not found"; exit 1; }
+
+which proxybroker &>/dev/null || { echo "proxybroker not found"; exit 1; }
+cd $proxies_dir || { echo "can't cd into proxies dir"; exit 1; }
+
+timeout 300 proxybroker find \
+    -o pbproxies.json \
+    -f json \
+    --types SOCKS5 SOCKS4 HTTP CONNECT:80 CONNECT:25 \
+    -l 250 \
+    --lvl High Anonymous

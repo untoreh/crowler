2e16801c29b2312aa2ea51bf1441703f92834af4
untoreh <francesco.giannelli@yahoo.it>
2022-12-05 18:48:52 +0100

    feat: simplified ads
    - insert banners instead of links
    - use `takeoverfields` instead of replacing objs, it is more orc friendly

--- src/nim/ads.nim
+++ src/nim/ads.nim
@@ -1,33 +1,35 @@
-import karax/[vdom, karaxdsl], strformat, locks, sugar, strutils, uri, parsexml,
-    streams, std/algorithm, macros
-import macros, chronos, chronos/asyncsync, htmlparser, xmltree
-import os
-import sets
-import cfg
-import utils
-import cache
-import html_entities
-import html_misc
+import std/[os, sets, htmlparser, xmltree, parsexml, strformat, strutils, locks,
+    sugar, streams, algorithm, macros]
+import karax/[vdom, karaxdsl]
+import chronos, chronos/asyncsync
+
+import types, cfg, utils, cache, html_entities, html_misc, cj
+
+export cj
 
 # NOTE: the space ' ' inside the `<script> </script>` tag is IMPORTANT to prevent `</>` tag collapsing, since it breaks html
 
+proc getVNode(): ptr VNode =
+  result = create(VNode)
+  result[] = newVNode(VNodeKind.verbatim)
+
 let
-  ADS_HEAD* = create(XmlNode)
-  ADS_HEADER* = create(XmlNode)
-  ADS_SIDEBAR* = create(XmlNode)
-  ADS_FOOTER* = create(XmlNode)
-  ADS_LINKS* = create(seq[string])
-  ADS_ARTICLES* = create(XmlNode)
-  ADS_RELATED* = create(XmlNode)
-  ADS_SEPARATOR* = create(XmlNode)
+  adsHead* = getVNode()
+  adsHeader* = getVNode()
+  adsSidebar* = getVNode()
+  adsFooter* = getVNode()
+  adsLinks* = create(seq[string])
+  adsArticles* = getVNode()
+  adsRelated* = getVNode()
+  adsSeparator* = getVNode()
 
 var locksInitialized: bool
 var adsFirstRead*, assetsFirstRead*: bool
-var adsHeadLock, adsHeaderLock, adsSidebarLock, adsFooterLock,
-  adsFooterLinksLock, adsLinksLock, adsArticlesLock, adsRelatedLock,
-    adsSeparatorLock: Lock
+var adsHeadLock*, adsHeaderLock*, adsSidebarLock*, adsFooterLock*,
+  adsLinksLock*, adsArticlesLock*, adsRelatedLock*,
+    adsSeparatorLock*: Lock
 
-var adsLinksCount, adsLinksIdx, adsFooterLinksCount, adsFooterLinksIdx: int
+var adsLinksCount, adsLinksIdx: int
 
 macro loadIfExists(basename: static[string], varname) =
   let path = DATA_ADS_PATH / basename
@@ -53,40 +55,45 @@ template initLinks(name, data) =
       data[].setLen(1)
       `name Count` = 1
 
-proc readAdsConfig*() =
+proc adsVNode(el: XmlNode): VNode {.gcsafe.} =
+  result = newVNode(VNodeKind.verbatim)
+  if not el.isnil:
+    for e in el:
+      result.add e.withClosingHtmlTag.verbatim
 
+proc readAdsConfig*() =
   withLock(adsHeadLock):
     let adsHeadFile = DATA_ADS_PATH / "head.html"
     if fileExists(adsHeadFile):
-      ADS_HEAD[] = loadHtml(adsHeadFile)
+      takeOverFields(loadHtml(adsHeadFile).adsVNode, adsHead[])
   withLock(adsHeaderLock):
     let adsHeaderFile = DATA_ADS_PATH / "header.html"
     if fileExists(adsHeaderFile):
-      ADS_HEADER[] = loadHtml(adsHeaderFile)
+      takeOverFields(loadHtml(adsHeaderFile).adsVNode, adsHeader[])
   withLock(adsSidebarLock):
     let adsSidebarFile = DATA_ADS_PATH / "sidebar.html"
     if fileExists(adsSidebarFile):
-      ADS_SIDEBAR[] = loadHtml(adsSidebarFile)
+      takeOverFields(loadHtml(adsSidebarFile).adsVNode, adsSidebar[])
   withLock(adsFooterLock):
     let adsFooterFile = DATA_ADS_PATH / "footer.html"
     if fileExists(adsFooterFile):
-      ADS_FOOTER[] = loadHtml(adsFooterFile)
+      takeOverFields(loadHtml(adsFooterFile).adsVNode, adsFooter[])
   # Links
   withLock(adsLinksLock):
     let adsLinksFile = DATA_ADS_PATH / "links.txt"
-    initLinks adsLinks, ADS_LINKS
+    initLinks adsLinks, adsLinks
   withLock(adsArticlesLock):
     let adsArticlesFile = DATA_ADS_PATH / "articles.html"
     if fileExists(adsArticlesFile):
-      ADS_ARTICLES[] = loadHtml(adsArticlesFile)
+      takeOverFields(loadHtml(adsArticlesFile).adsVNode, adsArticles[])
   withLock(adsRelatedLock):
     let adsRelatedFile = DATA_ADS_PATH / "related.html"
     if fileExists(adsRelatedFile):
-      ADS_RELATED[] = loadHtml(adsRelatedFile)
+      takeOverFields(loadHtml(adsRelatedFile).adsVNode, adsRelated[])
   withLock(adsSeparatorLock):
     let adsSeparatorFile = DATA_ADS_PATH / "separator.html"
     if fileExists(adsSeparatorFile):
-      ADS_SEPARATOR[] = loadHtml(adsSeparatorFile)
+      takeOverFields(loadHtml(adsSeparatorFile).adsVNode, adsSeparator[])
 
 template nextLink(name, data) =
   checkNil(data):
@@ -100,7 +107,7 @@ template nextLink(name, data) =
         `name Idx` = if `name Idx` >= `name Count`: 0 else: `name Idx` + 1
 
 proc nextAdsLink*(): Future[string] {.async.} =
-  nextLink adsLinks, ADS_LINKS
+  nextLink adsLinks, adsLinks
 
 type AdLinkType* = enum tags, footer
 type AdLinkStyle* = enum wrap, ico
@@ -141,109 +148,152 @@ template adLink*(kind; stl: static): auto =
 template adLink*(kind): auto =
   await adLinkFut(kind, static(AdLinkStyle.wrap))
 
-proc adsVNode(el: XmlNode): VNode {.gcsafe.} =
-  return el.withClosingHtmlTag.verbatim
+template withOutLock(l: Lock, code) =
+  try:
+    l.release()
+    code
+  finally:
+    l.acquire()
 
-proc insertAd*(name: ptr XmlNode): seq[VNode] {.gcsafe.} =
-  result = newSeq[VNode]()
-  if name.isnil or name[].isnil:
-    logall "ads: xmlnode is nil."
-  else:
-    for el in name[]:
-      if el.kind == xnElement:
-        result.add el.adsVNode
-
-iterator adsFrom*(loc: ptr XmlNode): VNode =
+iterator adsFromImpl(loc: ptr VNode, l: var Lock): VNode =
   if loc.isnil or loc[].isnil:
     discard
   else:
-    for el in loc[]:
-      yield el.adsVNode
+    withLock(l):
+      for el in loc[]:
+        withOutLock(l):
+          yield el
+
+template adsFrom*(name): VNode = adsFromImpl(name, `name Lock`)
 
 import generator
 export generator
 import std/importutils
-proc adsGen*(loc: ptr XmlNode): Generator[XmlNode, VNode] =
+proc adsGen*(loc: ptr VNode): Iterator[VNode] =
   if loc.isnil or loc[].isnil:
     discard
   else:
-    privateAccess(XmlNode)
-    result = newGen[XmlNode, VNode](loc[].s, adsVNode)
+    privateAccess(VNode)
+    result = newIter[VNode](loc[].kids)
 
-proc replaceLinks*(str: string, chunksize = 250): Future[string] {.async.} =
-  ## chunksize is the number of chars between links
+proc allSizeBanners*(topic: string, kws: seq[string] = @[],
+    lang: string = "English", vertical: static[bool] = false, class = "",
+        fallback: static[bool] = false, doClear: static[bool] = false): Future[
+    VNode] {.async.} =
+  var textLinks: Generator[XmlNode, string]
+  when fallback:
+    textLinks = await adsLinksGen(topic, kws, lang)
+  result = buildHtml(tdiv(class = fmt"ads-{class}"))
+  template banner(clr): string =
+    await getBanner(topic, kws, sz, lang, vertical, doClear = clr)
+  for sz in [des, tab, pho]:
+    let ad = buildHtml(tdiv(class = $sz))
+    var b = banner(doClear)
+    if b.len == 0:
+      b.add banner(true)
+    ad.add b.verbatim
+    when fallback:
+      if ad[0].len == 0:
+        ad[0].add textLinks.next().verbatim
+    result.add ad
 
-  checkNil(ADS_LINKS):
-    if len(ADS_LINKS[]) == 0 or ADS_LINKS[][0] == "":
-      return str
-  var maxsize = str.len
-  var chunkpos = if chunksize >= maxsize: maxsize.div(2)
-                else: chunksize
-  var positions: seq[int]
-  while chunkpos <= maxsize:
-    positions.add(chunkpos)
-    chunkpos += chunksize
-  positions.reverse()
+proc insertAds*(txt: string, charsInterval = 2500, topic = "", lang = "",
+    kws: seq[string] = @[]): Future[string] {.async.} =
+  ## Insert ads over `txt` after chunks of texts of specified `charsInterval` size.
+  # var linksGen = await adsLinksGen(topic, kws, lang, getter = cjHtml)
+  var lines = txt.splitLines()
+  var idx = 0
+  var chars = 0
+  while idx < lines.len:
+    chars += lines[idx].len
+    if chars > charsInterval:
+      let ad = await allSizeBanners(topic, kws, lang, class = "in-article",
+          fallback = true) # This is the first call, clear session
+      lines.insert($ad, idx)
+      chars = 0
+      idx.inc
+    idx.inc
+  return lines.join("\n")
 
-  var s = newStringStream(str)
-  if s == nil:
-    raise newException(CatchableError, "ads: cannot convert str into stream")
-  var x: XmlParser
-  var txtpos, prevstrpos, strpos: int
-  var filled: bool
-  open(x, s, "")
-  defer: close(x)
-  while true:
-    next(x)
-    case x.kind:
-      of xmlCharData:
-        let
-          txt = x.charData
-          txtStop = txt.len
-        prevstrpos = strpos
-        strpos = x.offsetBase + x.bufpos - txtStop
-        # add processed non text data starting from previous point
-        if strpos > prevstrpos:
-          let tail = str[prevstrpos..strpos - 1]
-          # FIXME: The xmlparser never appears to output `xmlEntity` events
-          # and deals with entities in a strange way
-          if (tail & ";").isEntity:
-            doassert txt[0] != str[strpos + 1]
-            result.add tail
-            result.add ';' # the xmlparser skips the semicolon
-            strpos += 1
-            continue
-          else:
-            result.add tail
-            if str[strpos] == '>': # FIXME: this shouldn't be required...
-               result.add '>'
-               strpos += 1
-        strpos += txtStop  # add the current text to the current string position
+# proc insertAds*(str: string, chunksize = 2500, lang = "", topic = "",
+#                   kws: seq[string] = @[], staticLinks: static[
+#                       bool] = false): Future[string] {.async.} =
+#   ## chunksize is the number of chars between ads
 
-        if unlikely(positions.len == 0):
-          result.add txt
-          continue
-        for (w, isSep) in txt.tokenize():
-          txtpos += w.len
-          if txtpos > positions[^1]:
-            if (not isSep) and (w.len > 5):
-              let link = buildhtml(a(href = (await nextAdsLink()),
-                  class = "ad-link")): text w
-              result.add $link
-              discard positions.pop()
-            else:
-              result.add w
-            if positions.len == 0:
-              if txtpos <= txt.len:
-                result.add txt[txtpos..^1]
-              break
-          else:
-            result.add w
-      of xmlEof:
-        break
-      else:
-        if filled:
-          break
+#   var linksGen =
+#     when staticLinks: adsGen(adsLinks)
+#     else: await adsLinksGen(topic, kws, lang, getter = url)
+#   if linksGen.len == 0:
+#     return str
+
+#   var maxsize = str.len
+#   var chunkpos = if chunksize >= maxsize: maxsize.div(2)
+#                 else: chunksize
+#   var positions: seq[int]
+#   while chunkpos <= maxsize:
+#     positions.add(chunkpos)
+#     chunkpos += chunksize
+#   positions.reverse()
+
+#   var s = newStringStream(str)
+#   if s == nil:
+#     raise newException(CatchableError, "ads: cannot convert str into stream")
+#   var x: XmlParser
+#   var txtpos, prevstrpos, strpos: int
+#   var filled: bool
+#   open(x, s, "")
+#   defer: close(x)
+#   while true:
+#     next(x)
+#     case x.kind:
+#       of xmlCharData:
+#         let
+#           txt = x.charData
+#           txtStop = txt.len
+#         prevstrpos = strpos
+#         strpos = x.offsetBase + x.bufpos - txtStop
+#         # add processed non text data starting from previous point
+#         if strpos > prevstrpos:
+#           let tail = str[prevstrpos..strpos - 1]
+#           # FIXME: The xmlparser never appears to output `xmlEntity` events
+#           # and deals with entities in a strange way
+#           if (tail & ";").isEntity:
+#             doassert txt[0] != str[strpos + 1]
+#             result.add tail
+#             result.add ';' # the xmlparser skips the semicolon
+#             strpos += 1
+#             continue
+#           else:
+#             result.add tail
+#             if str[strpos] == '>': # FIXME: this shouldn't be required...
+#               result.add '>'
+#               strpos += 1
+#         strpos += txtStop # add the current text to the current string position
+
+#         if unlikely(positions.len == 0):
+#           result.add txt
+#           continue
+#         for (w, isSep) in txt.tokenize():
+#           txtpos += w.len
+#           if txtpos > positions[^1]:
+#             if (not isSep) and (w.len > 5):
+#               let link = buildhtml(a(href = linksGen.next,
+#                   class = "ad-link")): text w
+#               result.add $link
+#               discard positions.pop()
+#             else:
+#               result.add w
+#             if positions.len == 0:
+#               if txtpos <= txt.len:
+#                 result.add txt[txtpos..^1]
+#               break
+#           else:
+#             result.add w
+#       of xmlEof:
+#         break
+#       else:
+#         if filled:
+#           break
 
 import fsnotify
 type WatchKind = enum
@@ -302,8 +352,7 @@ proc runAssetsWatcher*() =
 when isMainModule:
   import std/strtabs {.all.}
   import std/importutils
-  privateAccess(StringTableObj)
   runAdsWatcher()
-  for (key, val, hasValue) in ADS_HEAD[][2].attrs.data:
-    if hasValue:
-      echo key
+  # for n in adsHeader[]:
+  #
+  #   echo n

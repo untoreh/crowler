acdb810a265faf4a5e12ee542e6364f76bef112a
untoreh <francesco.giannelli@yahoo.it>
2023-01-10 19:07:03 +0100

    tweak: change container name

--- scripts/newsite.sh
+++ scripts/newsite.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 set -e
-container="scraper"
+container="server"
 
 docker exec -it $container python src/py/newsite.py "$@"
 docker exec -it $container scripts/supc.ch update
+docker exec -it $container scripts/supc.ch restart scraper
 killall -10 caddy # USR1

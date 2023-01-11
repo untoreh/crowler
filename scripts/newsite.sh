46e90e2aaf3b8eed6d60054aa516b39e2344941a
untoreh <francesco.giannelli@yahoo.it>
2023-01-11 11:26:44 +0100

    fix: caddy USR1 does not seem to work

--- scripts/newsite.sh
+++ scripts/newsite.sh
@@ -6,4 +6,5 @@ container="server"
 docker exec -it $container python src/py/newsite.py "$@"
 docker exec -it $container scripts/supc.ch update
 docker exec -it $container scripts/supc.ch restart scraper
-killall -10 caddy # USR1
+# killall -10 caddy # USR1
+killall -9 caddy # KILL

481c5e7a89489f1cc41282f8b3fcf7ad42ab3c5c
untoreh <francesco.giannelli@yahoo.it>
2023-01-12 09:48:54 +0100

    fix: typos

--- scripts/newsite.sh
+++ scripts/newsite.sh
@@ -4,7 +4,7 @@ set -e
 container="server"
 
 docker exec -it $container python src/py/newsite.py "$@"
-docker exec -it $container scripts/supc.ch update
-docker exec -it $container scripts/supc.ch restart scraper
+docker exec -it $container scripts/supc.sh update
+docker exec -it $container scripts/supc.sh restart scraper
 # killall -10 caddy # USR1
 killall -9 caddy # KILL
